0463a0a29e24f45ebd9aaf7dfe2aa7fcbe539884
idoscience <idoscience@users.noreply.github.com>
2014-12-20 16:49:13 +0600

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -2,12 +2,12 @@
 library(plyr)
 
 # Load files
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 # Merges the training and the test sets to create one data set
 x_all <- rbind(x_train, x_test)
@@ -15,13 +15,13 @@ y_all <- rbind(y_train, y_test)
 subject_all <- rbind(subject_train, subject_test)
 
 # Extracts only the measurements on the mean and standard deviation for each measurement
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 read_min_std_features <- grep("(mean|std)\\(\\)", features[,2])
 x_all <- x_all[, read_min_std_features]
 names(x_all) <- features[read_min_std_features,2]
 
 # Uses descriptive activity names to name the activities in the data set
-activitiy <- read.table("activity_labels.txt")
+activitiy <- read.table("UCI HAR Dataset/activity_labels.txt")
 y_all[,1] <- activity[y_all[,1],2]
 
 # Appropriately labels the data set with descriptive variable names
